807af290ee4f4478879a20c80aa2189bc517960c
Jacer OMRI <jaceromri@gmail.com>
2015-09-08 13:29:52 +0100

    Update odoo-prepare.sh

--- odoo-prepare.sh
+++ odoo-prepare.sh
@@ -1,5 +1,5 @@
 mkdir /nginx
 cp ./nginx.conf /nginx
 docker run -d -e POSTGRES_USER=odoo -e POSTGRES_PASSWORD=odoo --name db postgres
-docker run --name odoo --link db:db -t odoo -- --proxy-pass --db-filter=^%h$
+docker run --name odoo --link db:db -t odoo -- --proxy-mode --db-filter=^%h$
 docker run --link odoo:odoo --name nginx -v /nginx/nginx.conf:/etc/nginx/nginx.conf:ro -d -p 80:80 nginx
